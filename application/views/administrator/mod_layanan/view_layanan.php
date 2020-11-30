c6c580e5569d61694228224ad08d35da5d1fde48
almuhtadiaqil <almuhtadiaqil13@gmail.com>
2020-11-30 09:27:53 +0700

    profil

---
+++ application/views/administrator/mod_layanan/view_layanan.php
@@ -0,0 +1,27 @@
+<h2>Pengajuan Akun</h2>
+<table>
+    <thead>
+        <tr>No KK</tr>
+        <tr>NIK</tr>
+        <tr>Nama</tr>
+    </thead>
+    <tbody>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tbody>
+</table>
+
+<h2>Pengajuan Surat</h2>
+<table>
+    <thead>
+        <tr>NIK</tr>
+        <tr>Nama</tr>
+        <tr>Jenis Pengajuan</tr>
+    </thead>
+    <tbody>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tbody>
+</table>
